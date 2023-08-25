96f43f1945db10abb3dd06e9fbf3f631b485e8ee
SidSan97 <sidnei1.8santiago@hotmail.com>
2023-08-25 01:25:39 -0300

    Adcionando funçao de desconto

--- DiscountCalculatorTest.php
+++ DiscountCalculatorTest.php
@@ -1,14 +1,24 @@
 <?php
 
-class DiscountCalculatorTeste 
+class DiscountCalculatorTest
 {
-    public function ShouldApply_WhenValueIsAboveTheMinimum()
+    public function ShouldApply_WhenValueIsAboveTheMinimumTest()
     {
         $desconto = new DiscountCalculator;
         $totalValue = 130;
         $totalDescontado = $desconto->apply($totalValue);
 
         $valorEsperado = 110;
+        $this->assertEquals($valorEsperado, $totalDescontado);
+    }
+
+    public function ShouldNotApply_WhenValueIsBellowTheMinimumTest()
+    {
+        $desconto = new DiscountCalculator;
+        $totalValue = 90;
+        $totalDescontado = $desconto->apply($totalValue);
+
+        $valorEsperado = 90;
         $this->assertEquals($valorEsperado, $totalDescontado);
     }
 
